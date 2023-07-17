fefe9d7af6bc8a94db8c5da446cd92ab9c339e6a
joastek <joastek@gmail.com>
2023-07-17 21:17:26 +0200

    test

--- src/components/LoginPage/LoginButtons.tsx
+++ src/components/LoginPage/LoginButtons.tsx
@@ -5,13 +5,16 @@ const LoginButtons = () => {
     <>
       {" "}
       <div className="w-full flex p-16">
-        <button
-          type="button"
-          style={{ fontFamily: "Ultra" }}
-          className="m-auto  place-items-center text-white bg-gradient-to-br from-secondary to-third  font-light rounded-lg text-lg px-5 py-2.5 text-center "
-        >
-          <Link href="/dashboard"> Zaloguj jako gość </Link>
-        </button>
+        <Link href="/dashboard">
+          {" "}
+          <button
+            type="button"
+            style={{ fontFamily: "Ultra" }}
+            className="m-auto  place-items-center text-white bg-gradient-to-br from-secondary to-third  font-light rounded-lg text-lg px-5 py-2.5 text-center "
+          >
+            Zaloguj jako gość
+          </button>
+        </Link>
       </div>
     </>
   );
