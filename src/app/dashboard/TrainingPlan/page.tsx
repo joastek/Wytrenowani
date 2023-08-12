3166f59dae372725240bc2ced38eedfe1af7d62f
joastek <joastek@gmail.com>
2023-08-12 20:09:57 +0200

    diviscion base components

--- src/app/dashboard/TrainingPlan/page.tsx
+++ src/app/dashboard/TrainingPlan/page.tsx
@@ -1,22 +1,15 @@
 "use client";
 import { trainingState } from "@/types/type";
-import {
-  addTraining,
-  deleteTraining,
-  updateTraining,
-} from "@/slice/trainingList";
-import { addSet, deleteSet, updateSet } from "@/slice/trainingSet";
+import { addTraining } from "@/slice/trainingList";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
 import TextField from "@mui/material/TextField";
 import FormController from "@/components/TrainingPlan/FormControl";
+import TrainingItem from "@/components/TrainingPlan/AddActivity";
 
-import SeriesController from "@/components/TrainingPlan/seriesControl";
 const TrainingPlan = () => {
   const dispatch = useDispatch();
   const trainingList = useSelector(
@@ -29,12 +22,9 @@ const TrainingPlan = () => {
   const [addNewTraining, setAddNewTraining] = useState(false);
   const [name, setName] = useState("");
   const [username, setuserName] = useState("");
-
   const [selectedOption, setSelectedOption] = useState("");
   ////
-  const [seriesName, setSeriesName] = useState("");
-  const [series, setSeries] = useState("");
-  const [reps, setReps] = useState("");
+
   const handleAddNewTraining = () => {
     setAddNewTraining((prev) => !prev);
   };
@@ -55,20 +45,7 @@ const TrainingPlan = () => {
     );
     handleAddTypeOfTrening();
   };
-  const handleAddSet = (trainingId: any) => {
-    const lastSetId =
-      trainingSet.length > 0 ? trainingSet[trainingSet.length - 1].id : 0;
-    dispatch(
-      addSet({
-        id: lastSetId + 1,
-        seriesName: seriesName,
-        series: series,
-        reps: reps,
-        trainingId, // Dodajemy identyfikator treningu, do którego przypisujemy zestaw
-      })
-    );
-    // handleAddTypeOfTrening(); // Możesz zakomentować tę linię, aby okno dialogowe nie zamykało się po dodaniu zestawu
-  };
+
   return (
     <>
       <div className="w-screen justify-center text-center">
@@ -118,94 +95,13 @@ const TrainingPlan = () => {
             const setsForTraining = trainingSet.filter(
               (set: any) => set.trainingId === training.id
             );
-            // Pobieramy zestawy przypisane do tego treningu
-            return (
-              <div
-                key={training.id}
-                className=" w-[65rem]  m-auto bg-white mb-[1rem] relative rounded-lg"
-              >
-                <div className="flex w-full bg-slate-400 rounded-t-lg">
-                  {" "}
-                  <h1>{training.name}</h1>
-                  <h1>{training.selectedOption}</h1>
-                  <div className="ml-auto">
-                    {" "}
-                    <IconButton
-                      aria-label="delete"
-                      size="large"
-                      onClick={() => {
-                        dispatch(deleteTraining({ id: training.id }));
-                      }}
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </div>
-                </div>
 
-                <div className="block">
-                  {trainingSet.map((set: any) => {
-                    if (set.trainingId === training.id) {
-                      // Sprawdzamy, czy zestaw należy do tego treningu
-                      return (
-                        <div key={set.id} className="flex">
-                          <h1>{set.seriesName}</h1>
-                          <h1>{set.series}</h1>
-                          <h1>{set.reps}</h1>
-                          <IconButton
-                            aria-label="delete"
-                            size="large"
-                            onClick={() => {
-                              dispatch(deleteSet({ id: set.id }));
-                            }}
-                          >
-                            <DeleteIcon />
-                          </IconButton>
-                        </div>
-                      );
-                    } else {
-                      return null;
-                    }
-                  })}
-                  <Button
-                    variant="contained"
-                    onClick={() => handleAddSet(training.id)}
-                  >
-                    <AddIcon />
-                  </Button>
-                  <TextField
-                    id="outlined-basic"
-                    label="Nazwa serii"
-                    variant="outlined"
-                    onChange={(event: any) => {
-                      setSeriesName(event.target.value);
-                    }}
-                  />
-                  <SeriesController setSeries={setSeries} setReps={setReps} />
-                  {/* <input
-                    type="text"
-                    placeholder="new UserName..."
-                    onChange={(event: any) => {
-                      setNewuserName(event.target.value);
-                    }}
-                  />
-                  <button
-                    onClick={() => {
-                      dispatch(
-                        updateTraining({ id: user.id, username: newusername })
-                      );
-                    }}
-                  >
-                    Update Username
-                  </button>
-                  <button
-                    onClick={() => {
-                      dispatch(deleteTraining({ id: user.id }));
-                    }}
-                  >
-                    Delete username
-                  </button> */}
-                </div>
-              </div>
+            return (
+              <TrainingItem
+                key={training.id}
+                training={training}
+                trainingSet={setsForTraining}
+              />
             );
           })}
         </div>
---
+++ src/components/TrainingPlan/AddActivity.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { useState } from "react";
+import { deleteTraining } from "@/slice/trainingList";
+import { useDispatch } from "react-redux";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import TextField from "@mui/material/TextField";
+import SeriesController from "@/components/TrainingPlan/seriesControl";
+import { addSet, deleteSet } from "@/slice/trainingSet";
+
+const TrainingItem = ({ training, trainingSet }: any) => {
+  const dispatch = useDispatch();
+  const [seriesName, setSeriesName] = useState("");
+  const [series, setSeries] = useState("");
+  const [reps, setReps] = useState("");
+  const handleAddSet = (trainingId: any) => {
+    const lastSetId =
+      trainingSet.length > 0 ? trainingSet[trainingSet.length - 1].id : 0;
+    dispatch(
+      addSet({
+        id: lastSetId + 1,
+        seriesName: seriesName,
+        series: series,
+        reps: reps,
+        trainingId, // Dodajemy identyfikator treningu, do którego przypisujemy zestaw
+      })
+    );
+  };
+  return (
+    <div
+      key={training.id}
+      className=" w-[65rem]  m-auto bg-white mb-[1rem] relative rounded-lg"
+    >
+      <div className="flex w-full bg-slate-400 rounded-t-lg">
+        <h1>{training.name}</h1>
+        <h1>{training.selectedOption}</h1>
+        <div className="ml-auto">
+          <IconButton
+            aria-label="delete"
+            size="large"
+            onClick={() => {
+              dispatch(deleteTraining({ id: training.id }));
+            }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="block">
+        {trainingSet.map((set: any) => {
+          if (set.trainingId === training.id) {
+            return (
+              <div key={set.id} className="flex">
+                <h1>{set.seriesName}</h1>
+                <h1>{set.series}</h1>
+                <h1>{set.reps}</h1>
+                <IconButton
+                  aria-label="delete"
+                  size="large"
+                  onClick={() => {
+                    dispatch(deleteSet({ id: set.id }));
+                  }}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </div>
+            );
+          } else {
+            return null;
+          }
+        })}
+        <Button variant="contained" onClick={() => handleAddSet(training.id)}>
+          <AddIcon />
+        </Button>
+        <TextField
+          id="outlined-basic"
+          label="Nazwa serii"
+          variant="outlined"
+          onChange={(event) => {
+            setSeriesName(event.target.value);
+          }}
+        />
+        <SeriesController setSeries={setSeries} setReps={setReps} />
+      </div>
+    </div>
+  );
+};
+
+export default TrainingItem;
