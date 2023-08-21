691649137d2ba1fe0b08702ca495b24520891673
joastek <joastek@gmail.com>
2023-08-21 19:48:21 +0200

    feat-add basic table

--- src/app/dashboard/Food/page.tsx
+++ src/app/dashboard/Food/page.tsx
@@ -1,4 +1,28 @@
 const Food = () => {
-  return <>test</>;
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th className="mr-6">Produkt</th>
+                <th>Białko</th>
+                <th>Węglowoadny</th>
+                <th>Tłuszcze</th>
+                <th>Kcal</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>testtttt</td>
+                <td>testtttt</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 };
 export default Food;
