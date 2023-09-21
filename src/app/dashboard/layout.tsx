3e0ce52367fd12d5faf0218e02e4b0986955d7fe
joastek <joastek@gmail.com>
2023-09-21 22:05:14 +0200

    feat- add supabase to get accces google calednar API

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,11 +2,16 @@
 
 import { SidebarProvider } from "@/components/Dashboard/SideBarContext";
 import { Provider } from "react-redux";
-
+import { createClient } from "@supabase/supabase-js";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import store from "@/store/store";
 import Sidebar from "@/components/Dashboard/Sidebar";
 import AccountSidebar from "@/components/Dashboard/AccountSidebar";
 
+const supabase = createClient(
+  "https://stvkcwqqiszetklseils.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0dmtjd3FxaXN6ZXRrbHNlaWxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUzMTU1MTAsImV4cCI6MjAxMDg5MTUxMH0.sL41P_Y1oRDCLpx-PuzjUCO3--YPr0XcJ38Zrvw5Xmw"
+);
 export default function DashboardLayout1({
   children,
 }: {
@@ -15,11 +20,16 @@ export default function DashboardLayout1({
   return (
     <>
       <Provider store={store}>
-        <SidebarProvider>
-          <Sidebar />
-          {children}
-          <AccountSidebar />
-        </SidebarProvider>
+        <SessionContextProvider supabaseClient={supabase}>
+          {" "}
+          <SidebarProvider>
+            <Sidebar />
+
+            {children}
+
+            <AccountSidebar />
+          </SidebarProvider>{" "}
+        </SessionContextProvider>
       </Provider>
     </>
   );
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,41 +1,12 @@
 "use client";
-import { PieChart } from "@mui/x-charts/PieChart";
-import { useSelector } from "react-redux";
-import { RootState } from "@/types/type";
-import App from "@/components/TrainingPlan/test";
-const dashboard = () => {
-  const { result } = useSelector((state: RootState) => state.bmiCalculator);
-  console.log(result);
-  const data = [
-    { label: "Group A", value: 400 },
-    { label: "Group B", value: 300 },
-    { label: "Group C", value: 300 },
-    { label: "Group D", value: 200 },
-  ];
 
+const dashboard = () => {
   return (
     <>
       <main className="text-black bg-slate-100  h-24 w-screen text-right  dark:bg-secondary ">
         <div> </div>
         <div className="flex justify-center items-center  flex-col h-screen">
-          <div className="h-64 w-full bg-slate-400 p-4 flex justify-center items-center">
-            {result}
-            <App />
-            <PieChart
-              series={[
-                {
-                  paddingAngle: 5,
-                  innerRadius: 60,
-                  outerRadius: 80,
-                  data,
-                },
-              ]}
-              margin={{ right: 5 }}
-              width={200}
-              height={200}
-              legend={{ hidden: true }}
-            />
-          </div>
+          <div className="h-64 w-full bg-slate-400 p-4 flex justify-center items-center"></div>
         </div>
       </main>{" "}
     </>
